0c3de38f013989441405d2af7471d4ba792cdced
Allen Yang <allenyang@MacBook-Pro.local>
2020-08-17 21:08:32 -0700

    tf stuff

---
+++ model.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import sys
+from tensorflow.python.platform import gfile
+
+from tensorflow.core.protobuf import saved_model_pb2
+from tensorflow.python.util import compat
+
